b1d958d68da962af29341ce9779dceb1feac481e
xdd <45672942@qq.com>
2023-03-15 18:51:44 +0800

    增加comment接口

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { MenuModule } from './menu/menu.module';
 import { StudentModule } from './student/student.module';
+import { CommentModule } from './comment/comment.module';
 @Module({
   imports: [AuthModule, UsersModule, TypeOrmModule.forRoot({
     type: "mysql", //数据库类型
@@ -20,7 +21,7 @@ import { StudentModule } from './student/student.module';
     retryDelay:500, //重试连接数据库间隔
     retryAttempts:10,//重试连接数据库的次数
     autoLoadEntities:true, //如果为true,将自动加载实体 forFeature()方法注册的每个实体都将自动添加到配置对象的实体数组中
-  }), MenuModule, StudentModule],
+  }), MenuModule, StudentModule, CommentModule],
   controllers: [AppController],
   providers: [AppService]
 })
