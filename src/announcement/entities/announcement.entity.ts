7e07fae2b5d9b04ae6c994c4aead667e937612a7
xdd <45672942@qq.com>
2023-03-24 18:57:42 +0800

    feat(Announcement): 增加公告实体

--- src/announcement/entities/announcement.entity.ts
+++ src/announcement/entities/announcement.entity.ts
@@ -1,1 +1,10 @@
-export class Announcement {}
+import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+@Entity()
+export class Announcement {
+  @PrimaryGeneratedColumn()
+  id: number
+  @Column()
+  content: string
+  @Column()
+  create_time: string;
+}
